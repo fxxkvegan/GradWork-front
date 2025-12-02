801d439fab04321f161dabef0299e21db06dce21
heita <heiheydesu@gmail.com>
2025-12-02 12:38:29 +0900

    fix: 不要なインポートを削除

--- src/pages/ItemDetailPage.tsx
+++ src/pages/ItemDetailPage.tsx
@@ -50,7 +50,6 @@ import { API_CONFIG, createProductReview, fetchProductReviews } from "../api";
 // import { API_CONFIG } from "../constants/api";
 import { useAuth } from "../context/AuthContext";
 import { fetchCategories } from "../services/categoryApi";
-import productApi from "../services/productApi";
 import type { Category } from "../types/category";
 import type { Review } from "../types/review";
 
