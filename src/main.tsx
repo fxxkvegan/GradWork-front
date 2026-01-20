300d09bb9bded0ceb9afa97b35cada619286dc4d
gmizzz <kkusa114514@gmail.com>
2026-01-20 11:35:27 +0900

    feat: add design system styles for a modern UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./styles/design-system.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Layout from "./components/Layout";
 import { AuthProvider } from "./context/AuthContext";
